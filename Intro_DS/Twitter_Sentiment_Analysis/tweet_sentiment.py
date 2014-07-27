da2674138700bacae789be76dae36820d6b7a697
Kevin Payet <k_payet@yahoo.fr>
2014-07-27 16:28:54 +0200

    Problem 1 completed. These are the files

--- Intro_DS/Twitter_Sentiment_Analysis/tweet_sentiment.py
+++ Intro_DS/Twitter_Sentiment_Analysis/tweet_sentiment.py
@@ -1,17 +1,42 @@
 import sys
+import json
 
-def hw():
-    print 'Hello, world!'
+def sentiment(tweet, scores):
 
-def lines(fp):
-    print str(len(fp.readlines()))
+    out_score = 0
 
-def main():
-    sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    # create a list of words from the tweet
+    words = tweet.encode("utf-8").split()
 
-if __name__ == '__main__':
-    main()
+    for word in words:
+        if word in list(scores.keys()):
+            out_score += scores[word]
+
+    return out_score
+
+# create a dictionnary with the words and scores from AFINN-111.txt
+
+afinnfile = open("AFINN-111.txt")
+afin_scores = {}
+
+for line in afinnfile:
+    term, score = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
+    afin_scores[term] = int(score)  # Convert the score to an integer.
+
+# something like a loop over lines in output.json
+
+json_file = open("output.json")
+
+for line in json_file:
+
+    json_tree = json.loads(line)
+
+    if "lang" in list(json_tree.keys()):
+        if json_tree["lang"] != "en":
+            continue
+
+    if "text" in list(json_tree.keys()):
+        tweet = json_tree["text"]
+        tweet_score = sentiment(tweet, afin_scores)
+        print(tweet_score)
+
